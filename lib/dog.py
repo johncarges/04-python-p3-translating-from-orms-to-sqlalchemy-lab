6fd416e671bf74c478a326976f0de0df57d36e86
John Carges <johncarges@gmail.com>
2023-09-11 16:15:09 -0400

    passes all tests

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,30 @@
 from models import Dog
+from sqlalchemy import (create_engine, desc, func,
+    CheckConstraint, PrimaryKeyConstraint, UniqueConstraint,
+    Index, Column, DateTime, Integer, String)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-def create_table(base):
-    pass
+
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit
 
 def get_all(session):
-    pass
+    return session.query(Dog.name)
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter(Dog.name == name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter(Dog.id == id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog = find_by_id(session,dog.id)
+    dog.breed = breed
